d0e6483257abf4b40681dd274bf5fb51d1993941
Gabriel Soares <gabriel-soares@uergs.edu.br>
2023-05-01 14:57:52 -0300

    feat(fw): commenting some files to study

--- 6_WiFi/main/AccessPoint.c
+++ 6_WiFi/main/AccessPoint.c
@@ -13,48 +13,41 @@
 #include "freertos/task.h"
 //#include "esp_mac.h"
 
-
+// Define a tag for the logs
 #define TAG "Access Point"
 
+// Define constants for the access point (AP) configuration
+#define WIFI_AP_MODE_SSID           "ESP32_AP"    // Name of the AP
+#define WIFI_AP_MODE_PASS           "password"    // Password for the AP
+#define WIFI_AP_MODE_MAX_STA_CONN   10            // Maximum number of stations that can connect to the AP
+#define WIFI_AP_MODE_CHANNEL        1             // Wi-Fi channel to use for the AP
 
-#define WIFI_AP_MODE_SSID           "ESP32_AP"
-#define WIFI_AP_MODE_PASS           "password"
-#define WIFI_AP_MODE_MAX_STA_CONN   10
-#define WIFI_AP_MODE_CHANNEL        1
+// Event handler for Wi-Fi AP events
+void wifi_ap_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
 
-void wifi_ap_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
-    if(event_id == WIFI_EVENT_AP_START){
-        ESP_LOGI(TAG, "Access Point Started");
-    } else if(event_id == WIFI_EVENT_AP_STACONNECTED){
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(TAG, "Station "MACSTR" join, AID = %d, connected.", MAC2STR(event->mac), event->aid);
-    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED){
-        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        ESP_LOGI(TAG, "Station "MACSTR" join, AID = %d, disconnected.", MAC2STR(event->mac), event->aid);
-    } else if( event_id == WIFI_EVENT_AP_STOP){
-        ESP_LOGI(TAG, "Access Point Stop");
-    } else {
-        ESP_LOGI(TAG, "Unknow Event");
-    }
-}
-
-
+// Function to initialize the Wi-Fi AP
 void wifi_ap_init(void) {
-    
     ESP_LOGI(TAG, "Starting Access Point...");
 
+    // Initialize the TCP/IP network interface
     ESP_ERROR_CHECK(esp_netif_init());
+
+    // Create the default event loop and network interface for the Wi-Fi AP
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_ap();
 
+    // Initialize the Wi-Fi driver with default configuration
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
+    // Register the event handler for Wi-Fi events
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
                                                         &wifi_ap_event_handler,
                                                         NULL,
                                                         NULL));
+
+    // Configure the Wi-Fi AP with the specified settings
     wifi_config_t wifi_ap_config = {
         .ap = {
             .ssid = WIFI_AP_MODE_SSID,
@@ -65,9 +58,13 @@ void wifi_ap_init(void) {
             .authmode = WIFI_AUTH_WPA2_PSK,
         },
     };
+
+    // If no password is set, use an open Wi-Fi network
     if(strlen(WIFI_AP_MODE_PASS) == 0) {
         wifi_ap_config.ap.authmode = WIFI_AUTH_OPEN;
     }
+
+    // Set the Wi-Fi mode to AP and configure the AP with the specified settings
     ESP_LOGI(TAG, "Step 1...");
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
     ESP_LOGI(TAG, "Step 2...");
@@ -77,4 +74,24 @@ void wifi_ap_init(void) {
 
     ESP_LOGI(TAG, "Access Point Created\nSSID: %s\nPASSWORD: %s\nCANAL: %d\n",
         WIFI_AP_MODE_SSID, WIFI_AP_MODE_PASS, WIFI_AP_MODE_CHANNEL);
+}
+
+
+void wifi_ap_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    // Handle the event based on its ID
+    if(event_id == WIFI_EVENT_AP_START){
+        ESP_LOGI(TAG, "Access Point Started");
+    } else if(event_id == WIFI_EVENT_AP_STACONNECTED){
+        // A station connected to the AP
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "Station "MACSTR" join, AID = %d, connected.", MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED){
+        // A station disconnected from the AP
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "Station "MACSTR" join, AID = %d, disconnected.", MAC2STR(event->mac), event->aid);
+    } else if( event_id == WIFI_EVENT_AP_STOP){
+        ESP_LOGI(TAG, "Access Point Stop");
+    } else {
+        ESP_LOGI(TAG, "Unknow Event");
+    }
 }
--- 6_WiFi/main/main.c
+++ 6_WiFi/main/main.c
@@ -1,11 +1,3 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h> //DIR lib
@@ -26,30 +18,74 @@
 #define TAG "Main"
 
 #define WEB_MOUNT_POINT "www"
+
+void NVS(void);
+static void SPIFFS_Directory(char * path);
+esp_err_t SPIFFS_Mount(char * path, char * label, int max_files)
+
+void app_main(void)
+{
+    NVS();                                                        // Call the function to initialize non-volatile storage
+
+    //Start AP
+    wifi_ap_init();                                               // Call a function to initialize WiFi access point mode
+    ESP_ERROR_CHECK(start_rest_server(WEB_MOUNT_POINT));          // Call a function to start a REST server on a specific mount point
+
+    // Initialize SPIFFS
+    #define MAX_FILES 6
+    ESP_LOGI(TAG, "Initializing SPIFFS");                         // Log a message to indicate that SPIFFS is being initialized
+    if (SPIFFS_Mount("/html", "storage", MAX_FILES) != ESP_OK){   // Call a function to mount a SPIFFS partition with a specified path, label and maximum number of files, and check if it was successful
+      ESP_LOGE(TAG, "SPIFFS mount failed");                       // Log an error message if SPIFFS mount failed
+      while(1) { vTaskDelay(1); }                                 // Stay in an infinite loop to avoid the program from proceeding further
+    }
+}
+
+// This function initializes the Non-Volatile Storage (NVS) subsystem.
+// Which is a type of memory that can store data even when the device is powered off.
 void NVS(void){
-    //NVS é a sigla para armazenamento de armazenamento não volátil.
-  ESP_LOGI(TAG, "Init NVS");
-  esp_err_t ret = nvs_flash_init();
-  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    ret = nvs_flash_init();
-  }
-  ESP_ERROR_CHECK(ret);
-  ESP_LOGI(TAG, "NVS done\n");
+    // Log an informational message to the console using the "ESP_LOGI" macro.
+    ESP_LOGI(TAG, "Init NVS");
+
+    // Initialize the NVS subsystem by calling the "nvs_flash_init" function.
+    esp_err_t ret = nvs_flash_init();
+
+    // If the initialization failed due to no free pages or a new version being found,
+    // erase the NVS flash memory using the "nvs_flash_erase" function and attempt to initialize again.
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    // Check the return value of the initialization function using the "ESP_ERROR_CHECK" macro.
+    // If an error occurred during initialization, it will cause the program to abort and print an error message to the console.
+    ESP_ERROR_CHECK(ret);
+
+    // Log another informational message to the console indicating that NVS initialization is complete.
+    ESP_LOGI(TAG, "NVS done\n");
 }
 
+// Logs information about the SPIFFS partition specified by the "path" argument to the console.
 static void SPIFFS_Directory(char * path) {
-	DIR* dir = opendir(path);
-	assert(dir != NULL);
-	while (true) {
-		struct dirent*pe = readdir(dir);
-		if (!pe) break;
-		ESP_LOGI(TAG,"d_name=%s/%s d_ino=%d d_type=%x", path, pe->d_name,pe->d_ino, pe->d_type);
-	}
-	closedir(dir);
+    // Open the directory specified by the path argument
+    DIR* dir = opendir(path);
+    // Check that the directory was successfully opened
+    assert(dir != NULL);
+    // Loop through each file in the directory
+    while (true) {
+        // Read the next directory entry
+        struct dirent*pe = readdir(dir);
+        // If there are no more entries, break out of the loop
+        if (!pe) break;
+        // Log information about the file (name, inode number, type) using the ESP_LOGI function
+        ESP_LOGI(TAG,"d_name=%s/%s d_ino=%d d_type=%x", path, pe->d_name,pe->d_ino, pe->d_type);
+    }
+    // Close the directory
+    closedir(dir);
 }
 
+// Mounts the SPIFFS partition and initializes it for use in the program. 
 esp_err_t SPIFFS_Mount(char * path, char * label, int max_files) {
+    // Create a configuration structure for SPIFFS
 	esp_vfs_spiffs_conf_t conf = {
 		.base_path = path,
 		.partition_label = label,
@@ -57,14 +93,15 @@ esp_err_t SPIFFS_Mount(char * path, char * label, int max_files) {
 		.format_if_mount_failed =true
 	};
 
-	// Use settings defined above toinitialize and mount SPIFFS filesystem.
-	// Note: esp_vfs_spiffs_register is anall-in-one convenience function.
+	// Use the configuration to initialize and mount SPIFFS filesystem.
+	// Note: esp_vfs_spiffs_register is an all-in-one convenience function.
 	esp_err_t ret = esp_vfs_spiffs_register(&conf);
 
+	// Handle error cases if the mount operation fails
 	if (ret != ESP_OK) {
-		if (ret ==ESP_FAIL) {
+		if (ret == ESP_FAIL) {
 			ESP_LOGE(TAG, "Failed to mount or format filesystem");
-		} else if (ret== ESP_ERR_NOT_FOUND) {
+		} else if (ret == ESP_ERR_NOT_FOUND) {
 			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
 		} else {
 			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)",esp_err_to_name(ret));
@@ -72,6 +109,7 @@ esp_err_t SPIFFS_Mount(char * path, char * label, int max_files) {
 		return ret;
 	}
 
+	// Get information about the SPIFFS partition
 	size_t total = 0, used = 0;
 	ret = esp_spiffs_info(conf.partition_label, &total, &used);
 	if (ret != ESP_OK) {
@@ -80,27 +118,12 @@ esp_err_t SPIFFS_Mount(char * path, char * label, int max_files) {
 		ESP_LOGI(TAG,"Partition size: total: %d, used: %d", total, used);
 	}
 
+	// If the mount was successful, log the mount point and directory contents
 	if (ret == ESP_OK) {
 		ESP_LOGI(TAG, "Mount %s to %s success", path, label);
 		SPIFFS_Directory(path);
 	}
+
+	// Return the result of the mount operation
 	return ret;
 }
-
-void app_main(void)
-{
-    NVS();
-
-    //Start AP
-    wifi_ap_init();
-    ESP_ERROR_CHECK(start_rest_server(WEB_MOUNT_POINT));
-
-    // Initialize SPIFFS
-    #define MAX_FILES 6
-    ESP_LOGI(TAG, "Initializing SPIFFS");
-    if (SPIFFS_Mount("/html", "storage", MAX_FILES) != ESP_OK){
-      ESP_LOGE(TAG, "SPIFFS mount failed");
-      while(1) { vTaskDelay(1); }
-    }
-}
-
